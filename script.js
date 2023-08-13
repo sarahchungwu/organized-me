98b8605f5c3c7d11619f357ff68945fa5f56d33d
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-13 17:23:02 +1200

    fixed the createItemEl function

--- script.js
+++ script.js
@@ -53,13 +53,16 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log('columnEl:', columnEl)
-  console.log('column:', column)
-  console.log('item:', item)
-  console.log('index:', index)
+  // console.log('columnEl:', columnEl)
+  // console.log('column:', column)
+  // console.log('item:', item)
+  // console.log('index:', index)
   // List Item
   const listEl = document.createElement('li')
   listEl.classList.add('drag-item')
+  listEl.textContent = item
+  //Append
+  columnEl.appendChild(listEl)
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
