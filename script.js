0be396ccdb72527b929a5851c0bcd70eb9802813
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-13 17:52:09 +1200

    implementing ondrop ondragover onderagenter

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid')
 const addItemContainers = document.querySelectorAll('.add-container')
 const addItems = document.querySelectorAll('.add-item')
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list')
+const listColums = document.querySelectorAll('.drag-item-list')
 const backlogList = document.getElementById('backlog-list')
 const progressList = document.getElementById('progress-list')
 const completeList = document.getElementById('complete-list')
@@ -20,6 +20,7 @@ let onHoldListArray = []
 let listArray = []
 
 // Drag Functionality
+let draggedItem
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -61,6 +62,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li')
   listEl.classList.add('drag-item')
   listEl.textContent = item
+  listEl.draggable = true
+  listEl.setAttribute('ondragstart', 'drag(event)')
   //Append
   columnEl.appendChild(listEl)
 }
@@ -93,6 +96,27 @@ function updateDOM() {
     createItemEl(onHoldList, 0, onHoldItem, index)
   })
   // Run getSavedColumns only once, Update Local Storage
+}
+
+//When Item Starts Draggings
+function drag(e) {
+  draggedItem = e.target
+  console.log(draggedItem)
+}
+
+//Column Allows for Item to Drop
+function allowDrop(e) {
+  e.preventDefault()
+}
+
+//When Item Enters Column Area
+function dragEnter(column) {
+  console.log(listColums[column])
+}
+
+//Dropping Item in Column
+function drop(e) {
+  e.preventDefault()
 }
 
 //On Load
